d8134e2f9ad334bb5369ea49415fc3456ddbd25f
Muhammad Iqbal <mmuhammadiqbal963@gmail.com>
2026-03-03 08:13:55 +0700

    add : room model

---
+++ internal/repository/model/room_model.go
@@ -0,0 +1,30 @@
+package model
+
+import "time"
+
+type RoomModel struct {
+	ID        uint              `gorm:"primaryKey"`
+	Name      string            `gorm:"not null"`
+	Type      string            `gorm:"not null"`
+	CreatedBy uint              `gorm:"not null"`
+	Members   []RoomMemberModel `gorm:"foreignKey:RoomID"`
+	CreatedA  time.Time
+	UpdatedAt time.Time
+}
+
+func (RoomModel) TableName() string {
+	return "rooms"
+}
+
+type RoomMemberModel struct {
+	ID       uint      `gorm:"primaryKey"`
+	RoomID   uint      `gorm:"not null;index"`
+	UserID   uint      `gorm:"not null;index"`
+	User     UserModel `gorm:"foreignKey:UserID"`
+	Role     string    `gorm:"not null;default:'member'"`
+	JoinedAt time.Time
+}
+
+func (RoomMemberModel) TableName() string {
+	return "room_members"
+}
